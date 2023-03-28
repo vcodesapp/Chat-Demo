11e4a338e9e1496c3088970377bed6852cb1a600
Megan Yang <meimeipuppy@gmail.com>
2023-03-28 21:48:46 +0800

    Initial Commit

---
+++ Chat Demo/Chat_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_DemoApp.swift
+//  Chat Demo
+//
+//  Created by Mei Yang on 28/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Chat_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
